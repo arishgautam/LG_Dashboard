c1d767d9547e10d0058a9cefc9c4eddf0fecc7e9
arishgautam <arishgautam686@gmail.com>
2026-06-03 13:42:58 +0545

    first_commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center text-red-500">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
